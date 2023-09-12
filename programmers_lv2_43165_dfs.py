b8fda029e0dcd0bac4796b086e3be983cf9392a8
yujeong_yang@tmax.co.kr <yujeong_yang@tmax.co.kr>
2023-09-12 12:38:03 +0900

    [Programmers]: Lv 2. 타겟 넘버
     - Lv 2. 타겟 넘버
     - DFS

---
+++ programmers_lv2_43165_dfs.py
@@ -0,0 +1,16 @@
+def dfs_solution(numbers, target):
+    n = len(numbers)
+    count = 0
+
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal count
+                count += 1
+            return
+        else:
+            dfs(idx+1, result-numbers[idx])
+            dfs(idx+1, result+numbers[idx])
+
+    dfs(0, 0)
+    return count
