c1b5c510c78f81715c660c824fddd7625eea9336
YYuJeong <41742758+YYuJeong@users.noreply.github.com>
2023-10-04 15:30:04 +0000

    [Baekjoon] 2644번: 촌수계산
     - 촌수 계산
     - DFS / BFS

---
+++ Baekjoon/2644.py
@@ -0,0 +1,43 @@
+n = int(input())
+a, b = map(int, input().split())
+m = int(input())
+
+graph = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+res = [0] * (n+1)
+
+for _ in range(m):
+  x, y = map(int, input().split())
+  graph[x].append(y)
+  graph[y].append(x)
+
+def dfs(v):
+  visited[v] = True    
+  for i in graph[v]:
+    if not visited[i]:
+      res[i] = res[v] + 1
+      dfs(i)
+
+dfs(a)  
+
+if res[b] > 0:
+  print(res[b])
+else:
+  print(-1)
+
+# from collections import deque
+# q = deque([a])
+# visited[a] = True
+# res = [0]*(n+1)
+# while q:
+#   v = q.popleft()
+#   for i in graph[v]:
+#     if not visited[i]:
+#       q.append(i)
+#       visited[i] = True
+#       res[i] = res[v] + 1
+      
+# if res[b] > 0:
+#   print(res[b])
+# else:
+#   print(-1)
